fa7cb11697fc251212755ef95d5872268f5344e2
MehediHasanRemon <mahadiremon001@gmail.com>
2019-07-08 17:02:24 +0600

    Mahadi Changed

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-#Product List
+# Project Name : PyPrice
+# Author : Mehedi Hasan Remon
+# Facebook : facebook.com/believer.py
+
+
+
+########## Product List ############
 product = {"Bike":"$100","Car":"$200","Plane":"$300","Rocket":"$400","Space Ship":"$500"}
 
-#Print All Product
+######### Print Basic Instruction ##########
+print "\n"+"--== Mehedi Hasan Remon ==--"
+print "\n"+"Enter (x) to Exit From This Program"+"\n"
+
+############ Print All Product ############
 for x in product:
     print x
 
 
-#Main Function
+########### Main Function #############
 def cal(item,name):
     result = False
     for x in item:
-        if name == x:
+        if x == name:
             result = True
     return result
 
+#########Apply Our Main Function##########
+
+###### Exit Loop Function ######
+exit_var = False
+while exit_var == False:
+
+###### User Input #######
+    user_var = raw_input("\n"+"Enter Your  Product Name Here : ")
+
+###### Exit Loop Trigger ######
+    if user_var == "x":
+        exit_var = True
+
+     
+###### Show Price #######
+    if cal(product,user_var):
+        print user_var+" Price is",
+        print product[user_var]
+    
+####### If Input Dose Not Match #######
+    if cal(product,user_var) == False and user_var != "x":
+        print "This Product Dosent Exist on Our Store !"
